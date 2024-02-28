ba125e78302136c8d40589d04a00d39d34905daf
lucyJihyeon <141371734+lucyJihyeon@users.noreply.github.com>
2024-02-27 16:44:46 -0800

    solved activity 4

--- 01-Activities/04-Stu_Functional-Execution-Context/Unsolved/index.js
+++ 01-Activities/04-Stu_Functional-Execution-Context/Unsolved/index.js
@@ -1,13 +1,14 @@
 // returns the average value of an array of numbers
 function avg(array) {
+  var total = 0;
   function sum() {
-    let total = 0;
     for(let i = 0; i < array.length; i++) {
       total += array[i];
     }
+    return total
   }
   
-  return total/array.length;
+  return sum()/array.length
 }
 
 module.exports = avg;
