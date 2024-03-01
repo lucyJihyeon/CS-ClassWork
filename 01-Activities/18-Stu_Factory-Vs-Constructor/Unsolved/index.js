847cee370144097032dc42a88adf5f8670349e33
lucyJihyeon <141371734+lucyJihyeon@users.noreply.github.com>
2024-02-29 21:10:19 -0800

    activity 18 completed

--- 01-Activities/18-Stu_Factory-Vs-Constructor/Unsolved/index.js
+++ 01-Activities/18-Stu_Factory-Vs-Constructor/Unsolved/index.js
@@ -1,13 +1,31 @@
-// TODO: Refactor the class to a factory function.
-class Lesson {
-  constructor() {
-    this.title = 'Module 17 - Computer Science';
-    this.description = 'CS for JS';
-  }
-  information() {
-    console.log(this.title, this.description);
-  }
-}
+const { describe } = require("node:test");
 
-const csForJS = new Lesson();
-csForJS.information();
+// TODO: Refactor the class to a factory function.
+// class Lesson {
+//   constructor() {
+//     this.title = 'Module 17 - Computer Science';
+//     this.description = 'CS for JS';
+//   }
+//   information() {
+//     console.log(this.title, this.description);
+//   }
+// }
+
+
+const information = (classwork) => ({
+  info: () => 
+  console.log(classwork.title, classwork.description),
+});
+
+const Lesson = (title, description) => {
+  const classwork = {
+    title,
+    description,
+  };
+  return { ...information(classwork)}
+};
+
+// const csForJS = new Lesson();
+// csForJS.information();
+const csForJS = Lesson('Module 17 - Computer Science', 'CS for JS');
+csForJS.info()
--- 01-Activities/20-Stu_Event-Delegation-Handling/Unsolved/assets/js/script.js
+++ 01-Activities/20-Stu_Event-Delegation-Handling/Unsolved/assets/js/script.js
@@ -2,19 +2,26 @@ const buttons = document.getElementsByTagName('button');
 
 const clickHandler = function () {
 // TODO: Add a comment describing the purpose of this variable.
+// define variable count with a value of 0 to keep track of the number of times a button is clicked. 
   let count = 0;
 
   // TODO: Add a comment describing what is happening. 
+  //clickHandler function returns a new function 
   return function () {
   
    // TODO: Add a comment describing how this variable is being scoped. 
+   // the count value is incremented by 1 when the button is clicked. 
+   // and it is enclosed in the returned function 
     count++;
    // TODO: Add a comment describing how we are using the 'count' variable.
+   // this refers to the button that has been clicked and the text of the button is updated everytime when it is clicked. 
+   // it using template literal to convert count value to string 
     this.textContent = `Clicks: ${count}`;
   };
 };
 
 // TODO: Add a comment describing the purpose of this loop.
+// for every button elements, add an event listener to call clickHandler when it is clicked. 
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', clickHandler());
 }
