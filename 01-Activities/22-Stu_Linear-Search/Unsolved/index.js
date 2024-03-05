c932b5d506b289af4b7e99d89b1807faee74f059
lucyJihyeon <141371734+lucyJihyeon@users.noreply.github.com>
2024-03-04 19:10:19 -0800

    solved activity22; using while loop and for loop

--- 01-Activities/22-Stu_Linear-Search/Unsolved/index.js
+++ 01-Activities/22-Stu_Linear-Search/Unsolved/index.js
@@ -1,4 +1,23 @@
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 7];
 
 // TODO: Complete the linearSearch function below.
-const linearSearch = (array, element) => {};
+const linearSearch = (array, element) => {
+//    for (let i = 0; i < array.length; i++)   {
+//     if (array[i] == element)    {
+//         return i;
+//     } 
+//    }
+//    return -1;
+
+   let index = 0;
+   while (index < array.length) {
+    if (array[index] == element)    {
+        return index;
+    } else {
+        index++;
+    }
+   }
+   return -1;
+};
+
+module.exports = { linearSearch };
