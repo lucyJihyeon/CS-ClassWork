a809a021f626bd4bf9363df434f370ce34300cee
lucyJihyeon <141371734+lucyJihyeon@users.noreply.github.com>
2024-03-04 19:38:52 -0800

    activity 24

--- 01-Activities/24-Stu_Recursion/Unsolved/index.js
+++ 01-Activities/24-Stu_Recursion/Unsolved/index.js
@@ -1,13 +1,17 @@
 // TODO: Add a comment describing what the `position` parameter means for this function.
+//index of the fibonacci sequence. 
 const fibonacci = (position) => {
   // TODO: Add a comment describing the purpose of this conditional statement.
+  //the purpose of this if statement is to stop the recursion when it hits the first two values of the fibonacci sequence.
   if (position < 2) {
     return position;
   }
 
   // TODO: Add a comment describing the purpose of this return statement.
+  // Returns the sum of the Fibonacci numbers of the two previous positions.
   return fibonacci(position - 1) + fibonacci(position - 2);
 };
 
 // TODO: What will this return?
+// This will return the value of the Fibonacci sequence at the 9th position, which is 34.
 console.log(fibonacci(9));
