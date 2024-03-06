35f1aff58a01432db06bcda18dbe219678695c4f
lucyJihyeon <141371734+lucyJihyeon@users.noreply.github.com>
2024-03-05 16:32:39 -0800

    bubble-sort

--- 01-Activities/28-Stu_Mini-Project/Develop/01_bubble-sort/bubble-sort.js
+++ 01-Activities/28-Stu_Mini-Project/Develop/01_bubble-sort/bubble-sort.js
@@ -2,13 +2,38 @@
 const unsortedInputArray = [];
 
 // seed data in unsortedInputArray
-for (let i = 0; i < 2000; i++) {
-  unsortedInputArray.push(Math.round(Math.random() * 2000));
+for (let i = 0; i < 10; i++) {
+  unsortedInputArray.push(Math.round(Math.random() * 10));
 }
 
 // TODO: create bubble sort function
-const bubbleSort = (array) => {};
+const bubbleSort = (array) => {
+  
+  // for (let i = 1; i < array.length; i++) {
+  //   let cur = array[i];
+  //   let left = i - 1;
+  //   while (left >= 0 && array[left] > cur) {
+  //     array[left + 1] = array[left];
+  //     left--;
+  //   }
+  //   array[left + 1] = cur;
+  // }
+  // return array;
+  let swapped;
+  do {
+    swapped = false;
+    for ( let i = 0; i < array.length -1 ; i++) {
+      if (array[i] > array[i + 1])  {
+        let temp = array[i];
+        array[i] = array[i + 1];
+        array[i+1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  return array;
+};
 
 const sorted = bubbleSort(unsortedInputArray);
-console.log('Post Sort:', sorted.join(' '));
-console.log('DONE!');
+console.log("Post Sort:", sorted.join(" "));
+console.log("DONE!");
